3a8beac8bf96112c0600a16ecbfe9fd8a4004cd7
Your Name <louis.marmen.92@gmail.com>
2020-12-04 23:47:45 -0500

    Final Commit, Not talking to API

---
+++ CustomerPortal/Models/Building.cs
@@ -0,0 +1,17 @@
+namespace CustomerPortal.Models
+{
+
+
+    public class Building
+    {
+        // Properties
+        
+        public ulong Id { get; set; }
+
+        public ulong Customer_Id { get; set; }
+
+        public ulong Address_Id { get; set; }
+
+
+    }
+}
